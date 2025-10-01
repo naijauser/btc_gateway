8a21981aeb721127e61dba5815a272d9a17ef5b7
Nnamdi Aninye <unix1gl@gmail.com>
2025-10-01 07:46:03 +0100

    wrap swap logic in try catch

--- packages/nextjs/app/gateway/_components/Swap.tsx
+++ packages/nextjs/app/gateway/_components/Swap.tsx
@@ -5,71 +5,82 @@ import {
   StarknetInitializer,
   StarknetInitializerType,
 } from "@atomiqlabs/chain-starknet";
-import { BitcoinNetwork, FeeType, SwapperFactory } from "@atomiqlabs/sdk";
+import { BitcoinNetwork, FeeType, fromHumanReadableString, SwapperFactory } from "@atomiqlabs/sdk";
 
 export function Swap() {
   const swapTokens = async () => {
-    const Factory = new SwapperFactory<[StarknetInitializerType]>([
-      StarknetInitializer,
-    ] as const);
-    const Tokens = Factory.Tokens;
-    const nodeUrl = "https://starknet-sepolia.public.blastapi.io/rpc/v0_8";
-    const starknetRpcProvider = new RpcProviderWithRetries({
-      nodeUrl,
-    });
+    try {
+      const Factory = new SwapperFactory<[StarknetInitializerType]>([
+        StarknetInitializer,
+      ] as const);
+      const Tokens = Factory.Tokens;
+      const BTC_TOKEN = Tokens.BITCOIN.BTC
+      const STARKNET_TOKEN = Tokens.STARKNET.STRK;
+      const nodeUrl = "https://starknet-sepolia.public.blastapi.io/rpc/v0_8";
+      const starknetRpcProvider = new RpcProviderWithRetries({
+        nodeUrl,
+      });
 
-    const swapper = Factory.newSwapper({
-      chains: {
-        STARKNET: {
-          rpcUrl: starknetRpcProvider,
+      const swapper = Factory.newSwapper({
+        chains: {
+          STARKNET: {
+            rpcUrl: starknetRpcProvider,
+          },
         },
-      },
-      bitcoinNetwork: BitcoinNetwork.TESTNET4,
-    });
+        bitcoinNetwork: BitcoinNetwork.TESTNET4,
+      });
 
-    const swapLimits = swapper.getSwapLimits(
-      Tokens.BITCOIN.BTC,
-      Tokens.STARKNET.STRK,
-    );
-    console.log(
-      "Swap Limits, input min: " +
-        swapLimits.input.min +
-        " input max: " +
-        swapLimits.input.max,
-    ); // Immediately available
-    console.log(
-      "Swap Limits, output min: " +
-        swapLimits.output.min +
-        " output max: " +
-        swapLimits.output.max,
-    ); // Available after swap rejected due to too low/high amounts
+      const swapLimits = swapper.getSwapLimits(
+        BTC_TOKEN,
+        STARKNET_TOKEN,
+      );
+      console.log(
+        "Swap Limits, input min: " +
+          swapLimits.input.min +
+          " input max: " +
+          swapLimits.input.max,
+      ); // Immediately available
+      console.log(
+        "Swap Limits, output min: " +
+          swapLimits.output.min +
+          " output max: " +
+          swapLimits.output.max,
+      ); // Available after swap rejected due to too low/high amounts
 
-    // Create swap quote
-    const swap = await swapper.swap(
-      Tokens.BITCOIN.BTC, // Swap from BTC
-      Tokens.STARKNET.STRK, // Into STRK
-      3000n, // 3000 sats (0.00003 BTC)
-      true, // Whether we define an input or output amount
-      undefined, // Source address for the swaps, not used for swaps from BTC
-      "0x067b71c52c128cc94e466abd4b793fc02669bbd4336d9881a98c3aad83d3f710", // Destination address. TODO: Collect from user
-    );
+      const _exactIn = true; //exactIn = true, so we specify the input amount
+      const _amount = fromHumanReadableString("0.0001", BTC_TOKEN); // 0.0001 BTC
 
-    // Relevant data created about the swap
-    console.log("Swap created: " + swap.getId() + ":"); // Unique swap ID
-    console.log("   Input: " + swap.getInputWithoutFee()); // Input amount excluding fee
-    console.log("   Fees: " + swap.getFee().amountInSrcToken); // Fees paid on the output
-    for (let fee of swap.getFeeBreakdown()) {
-      console.log("     - " +  FeeType[fee.type] + ": " + fee.fee.amountInSrcToken); // Fees paid on the output
+      // Create swap quote
+      const swap = await swapper.swap(
+        BTC_TOKEN, // Swap from BTC
+        STARKNET_TOKEN, // Into STRK
+        _amount,
+        _exactIn,
+        undefined, // Source address for the swaps, not used for swaps from BTC
+        "0x067b71c52c128cc94e466abd4b793fc02669bbd4336d9881a98c3aad83d3f710", // Destination address. TODO: Collect from user
+        {
+          gasAmount: 1_000_000_000_000_000_000n //We can also request a gas drop on the destination chain (here requesting 1 STRK)
+        }
+      );
+
+      // Relevant data created about the swap
+      console.log("Swap created: " + swap.getId() + ":"); // Unique swap ID
+      console.log("   Input: " + swap.getInputWithoutFee()); // Input amount excluding fee
+      console.log("   Fees: " + swap.getFee().amountInSrcToken); // Fees paid on the output
+      for (let fee of swap.getFeeBreakdown()) {
+        console.log("     - " +  FeeType[fee.type] + ": " + fee.fee.amountInSrcToken); // Fees paid on the output
+      }
+      console.log("     Input with fees: " + swap.getInput()); // Total amount paid including fees
+      console.log("     Output: " + swap.getOutput()); // Output amount
+      console.log("     Quote expiry: " + swap.getQuoteExpiry()+ " (in " + (swap.getQuoteExpiry()-Date.now())/1000 + " seconds)"); // Quote expiry timestamp
+      console.log("     Price:"); // Pricing Information
+      console.log("       - swap: " + swap.getPriceInfo().swapPrice); // Price of the current swap (excluding fees)
+      console.log("       - market: " + swap.getPriceInfo().marketPrice); // Current Market price
+      console.log("       - difference: " + swap.getPriceInfo().difference); // Difference between swap price and the current market price
+      console.log("     Minimum bitcoin transaction fee rate " + swap.minimumBtcFeeRate + " sats/vB"); // Minimum fee rate of the bitcoin transaction
+    } catch (e) {
+      console.log(e);
     }
-    console.log("     Input with fees: " + swap.getInput()); // Total amount paid including fees
-    console.log("     Output: " + swap.getOutput()); // Output amount
-    console.log("     Quote expiry: " + swap.getQuoteExpiry()+ " (in " + (swap.getQuoteExpiry()-Date.now())/1000 + " seconds)"); // Quote expiry timestamp
-    console.log("     Price:"); // Pricing Information
-    console.log("       - swap: " + swap.getPriceInfo().swapPrice); // Price of the current swap (excluding fees)
-    console.log("       - market: " + swap.getPriceInfo().marketPrice); // Current Market price
-    console.log("       - difference: " + swap.getPriceInfo().difference); // Difference between swap price and the current market price
-    console.log("     Minimum bitcoin transaction fee rate" + swap.minimumBtcFeeRate + "sats/vB"); // Minimum fee rate of the bitcoin transaction
-    
   };
 
   return (
