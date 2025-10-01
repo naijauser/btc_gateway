5b64e83379f6ec40fd7a0e83b4cec77c810c42f9
Nnamdi Aninye <unix1gl@gmail.com>
2025-10-01 09:48:51 +0100

    start forming PSBT

--- packages/nextjs/app/gateway/_components/Swap.tsx
+++ packages/nextjs/app/gateway/_components/Swap.tsx
@@ -5,7 +5,7 @@ import {
   StarknetInitializer,
   StarknetInitializerType,
 } from "@atomiqlabs/chain-starknet";
-import { BitcoinNetwork, FeeType, fromHumanReadableString, SwapperFactory } from "@atomiqlabs/sdk";
+import { BitcoinNetwork, FeeType, fromHumanReadableString, SpvFromBTCSwapState, SwapperFactory } from "@atomiqlabs/sdk";
 
 export function Swap() {
   const swapTokens = async () => {
@@ -48,7 +48,7 @@ export function Swap() {
       ); // Available after swap rejected due to too low/high amounts
 
       const _exactIn = true; //exactIn = true, so we specify the input amount
-      const _amount = fromHumanReadableString("0.0001", BTC_TOKEN); // 0.0001 BTC
+      const _amount = 3000n; // 3000 sats (0.00003 BTC)
 
       // Create swap quote
       const swap = await swapper.swap(
@@ -57,16 +57,13 @@ export function Swap() {
         _amount,
         _exactIn,
         undefined, // Source address for the swaps, not used for swaps from BTC
-        "0x067b71c52c128cc94e466abd4b793fc02669bbd4336d9881a98c3aad83d3f710", // Destination address. TODO: Collect from user
-        {
-          gasAmount: 1_000_000_000_000_000_000n //We can also request a gas drop on the destination chain (here requesting 1 STRK)
-        }
+        "0x067b71c52c128cc94e466abd4b793fc02669bbd4336d9881a98c3aad83d3f710" // Destination address. TODO: Collect from user
       );
 
       // Relevant data created about the swap
       console.log("Swap created: " + swap.getId() + ":"); // Unique swap ID
       console.log("   Input: " + swap.getInputWithoutFee()); // Input amount excluding fee
-      console.log("   Fees: " + swap.getFee().amountInSrcToken); // Fees paid on the output
+      console.log("    Fees: " + swap.getFee().amountInSrcToken); // Fees paid on the output
       for (let fee of swap.getFeeBreakdown()) {
         console.log("     - " +  FeeType[fee.type] + ": " + fee.fee.amountInSrcToken); // Fees paid on the output
       }
@@ -78,6 +75,18 @@ export function Swap() {
       console.log("       - market: " + swap.getPriceInfo().marketPrice); // Current Market price
       console.log("       - difference: " + swap.getPriceInfo().difference); // Difference between swap price and the current market price
       console.log("     Minimum bitcoin transaction fee rate " + swap.minimumBtcFeeRate + " sats/vB"); // Minimum fee rate of the bitcoin transaction
+
+      // Add a listener for swap state changes
+      swap.events.on("swapState", (swap) => {
+        console.log("Swap " + swap.getId() + " changed state to " + SpvFromBTCSwapState[swap.getState()]);
+      });
+
+      // Obtain the funded PSBT (input already added) - ready for signing
+      const { psbt, signInputs } = await swap.getFundedPsbt({
+        address: "",
+        publicKey: "", // Public key for P2WPKH or P2TR outputs
+      });
+
     } catch (e) {
       console.log(e);
     }
