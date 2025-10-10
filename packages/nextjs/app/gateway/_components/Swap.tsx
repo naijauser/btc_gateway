c57032dee86f9a687a846b3a7455a821c13a28fa
Nnamdi Aninye <unix1gl@gmail.com>
2025-10-10 07:28:17 +0000

    separate swap details generation logic

--- packages/nextjs/app/gateway/_components/Swap.tsx
+++ packages/nextjs/app/gateway/_components/Swap.tsx
@@ -51,6 +51,138 @@ export function Swap() {
   const [toAmount, setToAmount] = useState("");
   const [showDetails, setShowDetails] = useState(false);
 
+  const generateSwapDetails = async () => {
+    try {
+      const Factory = new SwapperFactory<[StarknetInitializerType]>([
+        StarknetInitializer,
+      ] as const);
+      const Tokens = Factory.Tokens;
+      const BTC_TOKEN = Tokens.BITCOIN.BTC;
+      const STARKNET_TOKEN = Tokens.STARKNET.STRK;
+      const nodeUrl = "https://starknet-sepolia.public.blastapi.io/rpc/v0_8";
+      const starknetRpcProvider = new RpcProviderWithRetries({
+        nodeUrl,
+      });
+
+      let swo = await connect();
+      if (!swo) {
+        console.error(
+          "Xverse Wallet not found. Please install the Xverse Wallet extension.",
+        );
+        return;
+      }
+
+      const response = await request("wallet_connect", {
+        addresses: [
+          AddressPurpose.Ordinals,
+          AddressPurpose.Payment,
+          AddressPurpose.Stacks,
+          AddressPurpose.Starknet,
+          AddressPurpose.Spark,
+        ],
+      });
+
+      console.log(response);
+
+      if (response.status == "success") {
+        const paymentAddressItem = response.result.addresses.find(
+          (address) => address.purpose === AddressPurpose.Payment,
+        );
+
+        const starknetAddress = response.result.addresses.find(
+          (address) => address.purpose === AddressPurpose.Starknet,
+        );
+
+        const wallet = new StarknetSigner(swo as unknown as Account);
+        console.log("Wallet connected:", wallet);
+
+        const swapper = Factory.newSwapper({
+          chains: {
+            STARKNET: {
+              rpcUrl: starknetRpcProvider,
+            },
+          },
+          bitcoinNetwork: BitcoinNetwork.TESTNET4,
+        });
+        await swapper.init();
+
+        const swapLimits = swapper.getSwapLimits(BTC_TOKEN, STARKNET_TOKEN);
+        console.log(
+          "Swap Limits, input min: " +
+            swapLimits.input.min +
+            " input max: " +
+            swapLimits.input.max,
+        ); // Immediately available
+        console.log(
+          "Swap Limits, output min: " +
+            swapLimits.output.min +
+            " output max: " +
+            swapLimits.output.max,
+        ); // Available after swap rejected due to too low/high amounts
+
+        const _exactIn = true; //exactIn = true, so we specify the input amount
+        const _amount = 3000n; // 3000 sats (0.00003 BTC)
+
+        console.log("wallet address", wallet.account.address);
+        // Create swap quote
+        const swap = await swapper.swap(
+          BTC_TOKEN, // Swap from BTC
+          STARKNET_TOKEN, // Into STRK
+          _amount,
+          _exactIn,
+          undefined, // Source address for the swaps, not used for swaps from BTC
+          starknetAddress?.address as string, //TODO: Replace this with user collected address.
+        );
+
+        // Relevant data created about the swap
+        console.log("Swap created: " + swap.getId() + ":"); // Unique swap ID
+        console.log("   Input: " + swap.getInputWithoutFee()); // Input amount excluding fee
+        console.log("    Fees: " + swap.getFee().amountInSrcToken); // Fees paid on the output
+        for (let fee of swap.getFeeBreakdown()) {
+          console.log(
+            "     - " + FeeType[fee.type] + ": " + fee.fee.amountInSrcToken,
+          ); // Fees paid on the output
+        }
+        console.log("     Input with fees: " + swap.getInput()); // Total amount paid including fees
+        console.log("     Output: " + swap.getOutput()); // Output amount
+        console.log(
+          "     Quote expiry: " +
+            swap.getQuoteExpiry() +
+            " (in " +
+            (swap.getQuoteExpiry() - Date.now()) / 1000 +
+            " seconds)",
+        ); // Quote expiry timestamp
+        console.log("     Price:"); // Pricing Information
+        console.log("       - swap: " + swap.getPriceInfo().swapPrice); // Price of the current swap (excluding fees)
+        console.log("       - market: " + swap.getPriceInfo().marketPrice); // Current Market price
+        console.log("       - difference: " + swap.getPriceInfo().difference); // Difference between swap price and the current market price
+        console.log(
+          "     Minimum bitcoin transaction fee rate " +
+            swap.minimumBtcFeeRate +
+            " sats/vB",
+        ); // Minimum fee rate of the bitcoin transaction
+
+        // Add a listener for swap state changes
+        swap.events.on("swapState", (swap) => {
+          console.log(
+            "Swap " +
+              swap.getId() +
+              " changed state to " +
+              SpvFromBTCSwapState[swap.getState()],
+          );
+        });
+      } else {
+        if (response.error.code == RpcErrorCode.USER_REJECTION) {
+          console.error("User rejected wallet connection.", response.error);
+        } else {
+          console.error("Failed to connect to Xverse Wallet:", response.error);
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   const swapTokens = async () => {
     try {
       const Factory = new SwapperFactory<[StarknetInitializerType]>([
@@ -351,11 +483,11 @@ export function Swap() {
 
           {/* Swap button */}
           <button
-            onClick={() => swapTokens()}
+            onClick={() => generateSwapDetails()}
             disabled={false}
             className="w-full btn bg-btn-wallet text-primary-content font-semibold border-none py-3 rounded-full hover:opacity-90 transition-all"
           >
-            Swap
+            Generate Swap Details
           </button>
 
           {/* Wallet/network info */}
